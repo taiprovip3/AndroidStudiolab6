9831b6c3956b6d9fc30c94ae44eaf114d96a692c
taiproduaxe <taito1doraemon@gmail.com>
2022-03-28 13:57:45 +0700

    hinhanh o folder HINH_ANH

---
+++ app/src/main/java/com/example/myapplication/fragment_above.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_above extends Fragment {
+
+    public fragment_above() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_above, container, false);
+        return view;
+    }
+}
